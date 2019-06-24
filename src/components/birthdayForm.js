9192be0eab08dac7033aad021107636a91dedb2f
kaylan-johnson <drkaylan@gmail.com>
2019-06-24 16:37:00 -0600

    added clock component

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Datepicker from 'react-datepicker';
 import moment from 'moment'
+import Clock from './clock';
 
 class BirthdayForm extends Component {
     
@@ -9,7 +10,6 @@ class BirthdayForm extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleGenerate= this.handleGenerate.bind(this)
-        this.renderStuff= this.renderStuff.bind(this)
 
         this.state = {
             startDate: moment(),
@@ -33,31 +33,29 @@ class BirthdayForm extends Component {
         })
    }
 
-   renderStuff() {
-       if(this.state.formCompleted) {
-            return (
-                <h2>
-                    this is only appearing if the form haas been completed. show countdowntimer
-                        
-                </h2>
-            )
-       } else {
-           return (<h2>for has not been completed don' show countdown timer</h2>)
-       }
-   }
+
 
     render() {
         return (
             <div>
-                <h1>Birthday Form Component!</h1>
-                <Datepicker 
-                selected={this.startDate}
-                onChange={this.handleChange}
-                />
-                <a onClick={this.handleGenerate}>Generate Countdown </a>
                 {
-                    this.renderStuff()
+                    this.state.formCompleted ?
+                    <div>
+                        <Clock birthdayFormState={this.state}/>
+
+                    </div>
+                    :
+                    <div>
+                       <h1>Birthday Form Component!</h1>
+                        <Datepicker 
+                        selected={this.startDate}
+                        onChange={this.handleChange}
+                        />
+                        <a onClick={this.handleGenerate}>Generate Countdown </a>
+                
+                    </div>
                 }
+                
             </div>
         );
      
