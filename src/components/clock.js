fc57ccc5c3ca79d7f54f83a3de14132cd3d7ba33
kaylan-johnson <drkaylan@gmail.com>
2019-06-24 20:10:13 -0600

    implemented age feature

--- src/components/clock.js
+++ src/components/clock.js
@@ -39,6 +39,16 @@ class Clock extends Component {
     }
 }
 
+
+getAge = function() {
+    var bday = new Date(this.birthday);
+    var today = new Date();
+    var distance = today.getTime() - bday.getTime();
+    var daysOld = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var yearsOld = Number((daysOld/365).toFixed(0));
+    return yearsOld
+}.bind(this)
+
 componentDidMount() {
         this.timer = setInterval(() => {
             const timeRemaining = this.getTimeRemaining(this.birthday)
@@ -50,12 +60,17 @@ componentDidMount() {
     render() {  
         const data = this.state.timeRemaining
         return (
+        <div>
             <div>
                 <div>DAYS {data.days}</div>
                 <div>HRS {data.hours}</div>
                 <div>MINS {data.minutes}</div>
                 <div>SECS {data.seconds}</div>
             </div>
+            <div>
+                {<h4>remaining until you are {this.getAge()}</h4>}
+            </div>
+        </div>
         )
     }
 
