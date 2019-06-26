72c40905ddb979a324b3640d4d178eae066d9c1b
kaylan-johnson <drkaylan@gmail.com>
2019-06-26 16:42:10 -0600

    added flexbox to b-day form component

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Datepicker from 'react-datepicker';
-import moment from 'moment'
 import Clock from './clock';
 
 class BirthdayForm extends Component {
@@ -8,17 +7,14 @@ class BirthdayForm extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            startDate: new Date(),
+            formCompleted: false
+        };
+
         this.handleChange = this.handleChange.bind(this);
         this.handleGenerate= this.handleGenerate.bind(this);
-        this.handleChangeDate=this.handleChangeDate.bind(this);
-
-        this.state = {
-            startDate: moment(),
-            formCompleted: false
-        }
-
-
-        
+        this.handleChangeDate=this.handleChangeDate.bind(this);   
 
     }
 
@@ -44,23 +40,24 @@ class BirthdayForm extends Component {
 
     render() {
         return (
-            <div>
+            <div className="birthday-container">
                 {
                     this.state.formCompleted ?
-                    <div>
+                    <div className="clock-container">
                         <Clock birthdayFormState={this.state}/>
                         <a className="change-date" onClick={this.handleChangeDate}>Change Date</a>
 
 
                     </div>
                     :
-                    <div>
-                        <Datepicker 
-                        selected={this.startDate}
+                    <div className="date-picker-container">
+                        <Datepicker className="date-picker"
+                        selected={this.state.startDate}
                         onChange={this.handleChange}
                         />
+                        <div className="submit-container">
                         <a onClick={this.handleGenerate}>Generate Countdown</a>
-                
+                        </div>
                     </div>
                 }
                 
