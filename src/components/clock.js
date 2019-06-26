2237adc0d0162a387df30a543521357f5b2608bb
kaylan-johnson <drkaylan@gmail.com>
2019-06-26 14:28:27 -0600

    moved countdown tag

--- src/components/clock.js
+++ src/components/clock.js
@@ -104,18 +104,19 @@ componentDidMount() {
     render() {  
         const data = this.state.timeRemaining
         return (
-        <div className="countdown">
+        <div>
             {
                 this.state.timeRemaining == 0 ?
-
+                <div className="countdown">
                 <div className="message-container">
                     <p className="message-container__title">Countdown Complete</p>
                     <p className="message-container__message">HAPPY BIRTHDAY!</p>
                 </div>
+                </div>
 
                 :
             <div>
-                <div>
+                <div className="countdown">
                     <ul className="countdown__clock">
                         <li>DAYS<p>{data.days}</p></li>
                         <li>HRS<p>{data.hours}</p></li>
@@ -123,7 +124,7 @@ componentDidMount() {
                         <li>SECS<p>{data.seconds}</p></li>
                     </ul> 
                 </div>
-                <div>
+                <div className="until-container">
                 {this.renderMessage()}
                 </div>
             </div>
